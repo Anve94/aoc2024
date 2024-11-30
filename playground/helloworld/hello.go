98d85ee3162e7931194486d1a66d1c461e822384
Stefan Boonstra <boonstra.stefan.wp@gmail.com>
2024-11-30 22:14:06 +0100

    Put binary tree on the stack instead of the heap I guess

--- playground/helloworld/hello.go
+++ playground/helloworld/hello.go
@@ -8,7 +8,7 @@ import (
 
 func main() {
 	fmt.Println("Hello, world!")
-	tree := &binarytree.Tree{}
+	tree := binarytree.Tree{}
 	tree.Insert(5)
 	fmt.Println("The answer to the world, the universe and everything may be 42, but the root in this tree is " + strconv.Itoa(tree.Root.Data))
 }
